b39939793cc28bd68844216abc8763cbf12e1253
HiDE <msyariefhidayatulloh14@gmail.com>
2022-11-18 19:58:06 +0800

    Update AR tracking logic
    Now the AR object will match the tracking state of an image reference

--- Assets/Scripts/AR Environment/ImageTracking.cs
+++ Assets/Scripts/AR Environment/ImageTracking.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
 using System.Collections.Generic;
 
 namespace AlbeRt.AREnvironment
@@ -32,46 +33,65 @@ namespace AlbeRt.AREnvironment
 
         private void InstantiateNewPrefab(Object3D object3d)
         {
-            GameObject _newPrefab = Instantiate(object3d.Object, Vector3.zero, Quaternion.identity);
-            _newPrefab.SetActive(false);
+            GameObject _newPrefab = Instantiate(object3d.Object, transform);
             _newPrefab.name = object3d.Name;
+            _newPrefab.SetActive(false);
             _spawnedPrefabs.Add(object3d.Name, _newPrefab);
         }
 
         private void OnImageChanged(ARTrackedImagesChangedEventArgs args)
         {
-            foreach(ARTrackedImage _trackedImage in args.added)
-            {
-                UpdateImage(_trackedImage);
-            }
+            //foreach(ARTrackedImage _trackedImage in args.added)
+            //{
+            //    UpdateImage(_trackedImage);
+            //}
 
+            //foreach (ARTrackedImage _trackedImage in args.updated)
+            //{
+            //    UpdateImage(_trackedImage);
+            //}
+
+            //foreach (ARTrackedImage _trackedImage in args.removed)
+            //{
+            //    _spawnedPrefabs[_trackedImage.name].SetActive(false);
+            //}
             foreach (ARTrackedImage _trackedImage in args.updated)
             {
-                UpdateImage(_trackedImage);
-            }
+                string _trackedImageName = _trackedImage.referenceImage.name;
+                GameObject _trackedObject = _spawnedPrefabs[_trackedImageName];
 
-            foreach (ARTrackedImage _trackedImage in args.removed)
-            {
-                _spawnedPrefabs[_trackedImage.name].SetActive(false);
+                switch (_trackedImage.trackingState)
+                {
+                    case TrackingState.Tracking:
+                        _trackedObject.SetActive(true);
+                        _trackedObject.transform.position = _trackedImage.transform.position;
+                        break;
+                    case TrackingState.Limited:
+                        continue;
+                    case TrackingState.None:
+                        _trackedObject.SetActive(false);
+                        break;
+
+                }
             }
         }
 
         // https://forum.unity.com/threads/arfoundation-2-image-tracking-with-many-ref-images-and-many-objects.680518/#post-4915259
-        private void UpdateImage(ARTrackedImage trackedImage)
-        {
-            string _name = trackedImage.referenceImage.name;
-            Vector3 _position = trackedImage.transform.position;
+        //private void UpdateImage(ARTrackedImage trackedImage)
+        //{
+        //    string _name = trackedImage.referenceImage.name;
+        //    Vector3 _position = trackedImage.transform.position;
 
-            GameObject _prefab = _spawnedPrefabs[_name];
-            _prefab.transform.position = _position;
-            _prefab.SetActive(true);
+        //    GameObject _prefab = _spawnedPrefabs[_name];
+        //    _prefab.transform.position = _position;
+        //    _prefab.SetActive(true);
 
-            foreach(GameObject _go in _spawnedPrefabs.Values)
-            {
-                if (_go.name != _name)
-                    _go.SetActive(false);
-            }
-        }
+        //    foreach(GameObject _go in _spawnedPrefabs.Values)
+        //    {
+        //        if (_go.name != _name)
+        //            _go.SetActive(false);
+        //    }
+        //}
 
         
     }
